59dba6db869db0503e3ae46fecb9ec75aa813fd0
Chintapakdumdum <48258553+chintapakdumdum22@users.noreply.github.com>
2024-10-11 13:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,12 @@
 
 
 import os
-from config import Config
 from pyrogram import Client, idle
 import asyncio, logging
 import tgcrypto
 from pyromod import listen
 from logging.handlers import RotatingFileHandler
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID, SUDO_USERS, MONGO_URL, CHANNEL_ID, PREMIUM_LOGS  # Directly import required variables
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(
@@ -43,28 +43,31 @@ logging.basicConfig(
 )
 
 # Auth Users
-AUTH_USERS = [ int(chat) for chat in Config.AUTH_USERS.split(",") if chat != '']
+# If AUTH_USERS is available in os.environ, handle it as a string and convert to a list of integers
+AUTH_USERS = [int(chat) for chat in os.getenv("AUTH_USERS", "").split(",") if chat != '']
 
 # Prefixes 
 prefixes = ["/", "~", "?", "!"]
 
 plugins = dict(root="plugins")
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     bot = Client(
         "doms_extra_bot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
         sleep_threshold=20,
         plugins=plugins,
-        workers = 50
+        workers=50
     )
     
     async def main():
         await bot.start()
-        bot_info  = await bot.get_me()
+        bot_info = await bot.get_me()
         LOGGER.info(f"<--- @{bot_info.username} Started (c) LUCIFERBANKER --->")
         await idle()
-    
+
     asyncio.get_event_loop().run_until_complete(main())
-    LOGGER.info(f"<---Bot Stopped-->")
+    LOGGER.info(f"<---Bot Stopped--->")
+
